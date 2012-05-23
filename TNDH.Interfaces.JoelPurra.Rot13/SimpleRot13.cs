8f71053a564533e906cedb2c2ddeb331b71f6e52
Joel Purra <code+github@joelpurra.se>
2012-05-23 19:58:56 -0400

    Initial commit

---
+++ TNDH.Interfaces.JoelPurra.Rot13/SimpleRot13.cs
@@ -0,0 +1,19 @@
+namespace TNDH.Interfaces.JoelPurra.Rot13
+{
+	using System;
+	using TDNH;
+
+	public class SimpleRot13 : IInformation
+	{
+		public SimpleRot13 ()
+		{
+		}
+
+		#region IInformation implementation
+		public string Transform (string incoming)
+		{
+			throw new System.NotImplementedException ();
+		}
+		#endregion
+	}
+}
